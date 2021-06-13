26dd19f1d6e96c905c9e44481a7a6ab044a99f7f
arsyfpro <udonorewakoneko@gmail.com>
2021-06-14 04:52:30 +0700

    update database

--- petbnb.sql
+++ petbnb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 13, 2021 at 12:03 AM
+-- Generation Time: Jun 13, 2021 at 11:49 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
@@ -41,7 +41,6 @@ CREATE TABLE `host` (
 INSERT INTO `host` (`id_host`, `id_user`, `alamat`, `verified`) VALUES
 (2, 3, 'Jl. Tenggara Medan Kota', 'Y'),
 (3, 6, 'Jalan Cinta Medan Baru', 'Y'),
-(6, 8, 'lOHHH HEEEE', 'Y'),
 (7, 9, 'Jauh banget jalan Conjuring', 'Y'),
 (8, 10, 'Jl. Medan Sunggal', 'Y');
 
@@ -69,8 +68,7 @@ INSERT INTO `host_house` (`id_house`, `id_host`, `no_hp`, `email`, `foto`, `harg
 (1, 2, '081234567890', 'putripethouse@example.com', 'host1.jpg', 100000, 'Y'),
 (2, 3, '08080823456', 'email@email.com', 'host2.jpg', 120000, 'Y'),
 (3, 8, '0878896545', 'mhmd@email.com', 'host3.jpg', 95000, 'Y'),
-(4, 7, '12347859522', 'hosthewan@email.com', 'host4.jpg', 80000, 'N'),
-(11, 6, '0822758147565', 'yes@mail.com', '72406.jpg', 50000, 'Y');
+(4, 7, '12347859522', 'hosthewan@email.com', 'host4.jpg', 80000, 'N');
 
 -- --------------------------------------------------------
 
@@ -102,18 +100,19 @@ CREATE TABLE `pesanan` (
 --
 
 INSERT INTO `pesanan` (`id_pesanan`, `id_user`, `id_host`, `nama_hewan`, `jenis`, `umur`, `makanan_fav_hewan`, `catatan`, `layanan`, `check_in`, `check_out`, `voucher`, `total`, `total_bayar`, `status`, `waktu_pesan`) VALUES
-(1, 1, 2, 'Coocoon', 'Kucing', 2, 'Ikan', '', 'Normal', '', '2021-06-05', 'YNTKTS', 1, 0, '', ''),
-(2, 4, 2, 'Bernard', 'Anjing', 2, 'Royal Canin', '', 'Premium', '', '', 'N', 0, 0, '', ''),
-(3, 5, 2, 'Abu', 'Kucing', 1, 'Whiskas Tuna', '', 'Premium', '', '', 'N', 0, 0, '', ''),
-(4, 1, 2, 'Eren Yeager', 'Anjing', 19, 'Ayam', '', 'Pertalite', '', '', 'YNTKTS', 0, 0, '', ''),
-(5, 1, 3, 'Melinda', 'Burung', 1, 'Kacang', '', 'Premium', '14-06-2021', '16-06-2021', 'N', 270000, 270000, 'Belum', ''),
-(6, 1, 2, 'Kola', 'Kucing', 2, 'Ikan', 'Tolong Kola selalu dimandikan dua kali ya', 'Deluxe', '16-06-2021', '18-06-2021', 'N', 260000, 260000, 'Belum', ''),
-(7, 4, 8, 'Mendo', 'Anjing', 3, 'Ayam Cincang', '', 'Pertalite', '13-06-2021', '14-06-2021', 'YNTKTS', 108000, 108000, 'Belum', ''),
-(8, 4, 8, 'Melinda', 'Kucing', 2, 'Kacang Merah', '-', 'Normal', '14-06-2021', '15-06-2021', 'N', 95000, 95000, 'Belum', ''),
-(9, 4, 8, 'Merinda', 'Hamster', 1, 'Kacang', 'hehe', 'Pertalite', '17-06-2021', '18-06-2021', 'YNTKTS', 135000, 108000, 'Belum', ''),
-(10, 4, 8, 'Felix', 'Kucing', 2, 'asd', 'asdas', 'Pertalite', '24-06-2021', '25-06-2021', 'N', 135000, 135000, 'Selesai', ''),
+(1, 1, 2, 'Coocoon', 'Kucing', 2, 'Ikan', '-', 'Normal', '16-06-2021', '17-06-2021', 'YNTKTS', 270000, 270000, 'Belum', '10-06-2021 12:40'),
+(2, 4, 2, 'Bernard', 'Anjing', 2, 'Royal Canin', '-', 'Premium', '16-06-2021', '17-06-2021', 'N', 270000, 270000, 'Belum', '10-06-2021 14:33'),
+(3, 5, 2, 'Abu', 'Kucing', 1, 'Whiskas Tuna', '-', 'Premium', '16-06-2021', '17-06-2021', 'N', 270000, 270000, 'Belum', '10-06-2021 14:40'),
+(4, 1, 2, 'Eren Yeager', 'Anjing', 19, 'Ayam', '-', 'Pertalite', '16-06-2021', '17-06-2021', 'YNTKTS', 270000, 270000, 'Belum', '11-06-2021 11:40'),
+(5, 1, 3, 'Melinda', 'Burung', 1, 'Kacang', '-', 'Premium', '14-06-2021', '16-06-2021', 'N', 270000, 270000, 'Belum', '11-06-2021 12:40'),
+(6, 1, 2, 'Kola', 'Kucing', 2, 'Ikan', 'Tolong Kola selalu dimandikan dua kali ya', 'Deluxe', '16-06-2021', '18-06-2021', 'N', 260000, 260000, 'Belum', '11-06-2021 13:40'),
+(7, 4, 8, 'Mendo', 'Anjing', 3, 'Ayam Cincang', '-', 'Pertalite', '13-06-2021', '14-06-2021', 'YNTKTS', 108000, 108000, 'Belum', '11-06-2021 14:40'),
+(8, 4, 8, 'Melinda', 'Kucing', 2, 'Kacang Merah', '-', 'Normal', '14-06-2021', '15-06-2021', 'N', 95000, 95000, 'Belum', '11-06-2021 14:44'),
+(9, 4, 8, 'Merinda', 'Hamster', 1, 'Kacang', 'hehe', 'Pertalite', '17-06-2021', '18-06-2021', 'YNTKTS', 135000, 108000, 'Belum', '12-06-2021 13:44'),
+(10, 4, 8, 'Felix', 'Kucing', 2, 'asd', 'asdas', 'Pertalite', '24-06-2021', '25-06-2021', 'N', 135000, 135000, 'Selesai', '12-06-2021 14:40'),
 (11, 1, 2, 'Mento', 'Kucing', 2, 'Ayam', '-', 'Premium', '17-06-2021', '18-06-2021', 'N', 115000, 115000, 'Selesai', '12-06-2021 14:44'),
-(12, 1, 3, 'Messi', 'Ikan', 1, 'Kacang', '-', 'Deluxe', '15-06-2021', '16-06-2021', 'N', 150000, 150000, 'Selesai', '12-06-2021 21:49');
+(12, 1, 3, 'Messi', 'Ikan', 1, 'Kacang', '-', 'Deluxe', '15-06-2021', '16-06-2021', 'N', 150000, 150000, 'Selesai', '12-06-2021 21:49'),
+(13, 1, 3, 'Kiara', 'Ayam', 2, 'cacing', '-', 'Deluxe', '16-06-2021', '17-06-2021', 'N', 150000, 150000, 'Belum', '14-06-2021 04:43');
 
 -- --------------------------------------------------------
 
@@ -161,12 +160,11 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id`, `nama`, `username`, `password`, `user_level`) VALUES
 (1, 'sity fadia', 'fadia', 'fadia', 1),
-(2, 'arsya', 'arsya', 'arsya', 3),
+(2, 'admin', 'admin', 'admin', 3),
 (3, 'Putri Pet House', 'putri', 'putri', 2),
 (4, 'haya', 'haya', 'haya', 1),
 (5, 'ara', 'ara', 'ara', 1),
 (6, 'Haya Pet House', 'sity', 'sity', 2),
-(8, 'Gugukers', 'felis', '123', 2),
 (10, 'Mahmud Pet House', 'mhmd', '123', 2),
 (11, 'Arsya Fikri', 'arsyf', '123456', 1),
 (12, '123', '123', '123', 1),
@@ -254,7 +252,7 @@ ALTER TABLE `host_house`
 -- AUTO_INCREMENT for table `pesanan`
 --
 ALTER TABLE `pesanan`
-  MODIFY `id_pesanan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_pesanan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `review_rating_host`
@@ -272,7 +270,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `voucher`
 --
 ALTER TABLE `voucher`
-  MODIFY `id_voucher` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_voucher` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
